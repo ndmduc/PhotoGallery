2412ead67baabc22a48235c136935def233acf39
Duc Nguyen <ducdmn@outlook.com>
2018-11-04 10:54:04 +0700

    Init project

---
+++ src/PhotoGallery/Infrastructure/PhotoGalleryContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PhotoGallery.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.Infrastructure
+{
+    public class PhotoGalleryContext : DbContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        public DbSet<Error> Errors { get; set; }
+
+        public PhotoGalleryContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.Relational().TableName = entity.DisplayName();
+            }
+
+            // Photos
+            modelBuilder.Entity<Photo>().Property(p => p.Title).HasMaxLength(100);
+            modelBuilder.Entity<Photo>().Property(p => p.AlbumId).IsRequired();
+
+            // Album
+            modelBuilder.Entity<Album>().Property(p => p.Title).HasMaxLength(100);
+            modelBuilder.Entity<Album>().Property(p => p.Description).HasMaxLength(500);
+            modelBuilder.Entity<Album>().HasMany(p => p.Photos).WithOne(p=>p.Album);
+
+            // User
+            modelBuilder.Entity<User>().Property(p => p.Username).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<User>().Property(p => p.Email).IsRequired().HasMaxLength(200);
+            modelBuilder.Entity<User>().Property(p => p.HashedPassword).IsRequired().HasMaxLength(200);
+            modelBuilder.Entity<User>().Property(p => p.Salt).IsRequired().HasMaxLength(200);
+
+            // UserRole
+            modelBuilder.Entity<UserRole>().Property(ur => ur.UserId).IsRequired();
+            modelBuilder.Entity<UserRole>().Property(ur => ur.RoleId).IsRequired();
+
+            // Role
+            modelBuilder.Entity<Role>().Property(r => r.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ src/PhotoGallery/Infrastructure/Repositories/EntityBaseRepository.cs
@@ -0,0 +1,115 @@
+﻿using PhotoGallery.Entities;
+using PhotoGallery.Infrastructure.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhotoGallery.Infrastructure.Repositories
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private PhotoGalleryContext context;
+
+        public EntityBaseRepository(PhotoGalleryContext cont)
+        {
+            this.context = cont;
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return this.context.Set<T>().AsEnumerable();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await this.context.Set<T>().ToListAsync();
+        }
+
+        public virtual IEnumerable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = this.context.Set<T>();
+            foreach (var item in includeProperties)
+            {
+                query = query.Include(item);
+            }
+
+            return query.AsEnumerable();
+        }
+
+        public virtual async Task<IEnumerable<T>> AllIncludingAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = this.context.Set<T>();
+            foreach (var item in includeProperties)
+            {
+                query = query.Include(item);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate)
+        {
+            return this.context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public T GetSingle(int id)
+        {
+            return this.context.Set<T>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable query = this.context.Set<T>();
+            foreach (var item in includeProperties)
+            {
+                query =
+            }
+        }
+
+        public Task<T> GetSingleAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindByAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+       
+    }
+}
