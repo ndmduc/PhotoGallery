e4b98d662618e21e3adc0e9c4b8cef1f24c81abc
Duc Nguyen <ducdmn@outlook.com>
2018-12-26 09:05:55 +0700

    init pro

---
+++ src/PhotoGallery/Infrastructure/DbInitializer.cs
@@ -0,0 +1,114 @@
+﻿using PhotoGallery.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.Infrastructure
+{
+    public static class DbInitializer
+    {
+        private static PhotoGalleryContext context;
+
+        public static void Initialize(IServiceProvider serviceProvider, string imagesPath)
+        {
+            context = (PhotoGalleryContext)serviceProvider.GetService(typeof(PhotoGalleryContext));
+
+        }
+
+        private static void InitializePhotoAlbums(string imagesPath)
+        {
+            if (!context.Albums.Any())
+            {
+                List<Album> albums = new List<Album>();
+                var _album1 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 1",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+
+                var _album2 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 2",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+
+                var _album3 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 3",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+
+                var _album4 = context.Albums.Add(
+                    new Album
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Album 4",
+                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    }).Entity;
+
+                albums.Add(_album1);
+                albums.Add(_album2);
+                albums.Add(_album3);
+                albums.Add(_album4);
+
+                string[] images = Directory.GetFiles(Path.Combine(imagesPath, "images"));
+                Random rnd = new Random();
+
+                foreach (var image in images)
+                {
+                    int selectedAlbum = rnd.Next(1, 4);
+                    string fileName = Path.GetFileName(image);
+
+                    context.Photos.Add(new Photo
+                    {
+                        Title = fileName,
+                        DateUploaded = DateTime.Now,
+                        Uri = fileName,
+                        Album = albums.ElementAt(selectedAlbum)
+                    });
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private static void InitializeUserRoles()
+        {
+            if (!context.Roles.Any())
+            {
+                context.Roles.AddRange(new Role[] { new Role { Name = "Admin" } });
+                context.SaveChanges();
+            }
+
+            if (!context.Users.Any())
+            {
+                context.Users.Add(new User
+                {
+                    Email = "ndminhduc@gmail.com",
+                    Username = "duc",
+                    HashedPassword = "9wsmLgYM5Gu4zA/BSpxK2GIBEWzqMPKs8wl2WDBzH/4=",
+                    Salt = "GTtKxJA6xJuj3ifJtTXn9Q==",
+                    IsLocked = false,
+                    DateCreated = DateTime.Now
+                });
+
+                // create user admin for duc
+                context.UserRoles.AddRange(new UserRole[] {
+                    new UserRole {
+                        RoleId = 1,
+                        UserId=1}
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- src/PhotoGallery/Startup.cs
+++ src/PhotoGallery/Startup.cs
@@ -1,37 +1,142 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using PhotoGallery.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using PhotoGallery.Infrastructure.Repositories;
+using PhotoGallery.Infrastructure.Services;
+using System.Security.Claims;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+using PhotoGallery.Infrastructure.Repositories.Abstract;
+using PhotoGallery.Infrastructure.Services.Abstract;
 
 namespace PhotoGallery
 {
     public class Startup
     {
+        private static string applicationPath = string.Empty;
+
+        private static string contentRootPath = string.Empty;
+
+        public Startup(IHostingEnvironment env)
+        {
+            applicationPath = env.WebRootPath;
+            contentRootPath = env.ContentRootPath;
+
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(contentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //string sqlConnectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            //bool useInMemoryProvider = bool.Parse(Configuration["Data:PhotoGalleryConnection:InMemoryProvider"]);
+
+            //services.AddDbContext<PhotoGalleryContext>(options =>
+            //{
+            //    switch (useInMemoryProvider)
+            //    {
+            //        case true:
+            //            options.UseInMemoryDatabase();
+            //            break;
+            //        default:
+            //            options.UseSqlServer(sqlConnectionString);
+            //            break;
+            //    }
+            //});
+
+            services.AddDbContext<PhotoGalleryContext>(opt =>
+                                    opt.UseSqlServer(Configuration["Data:PhotoGalleryConnection:ConnectionString"]));
+            // Repositories
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+            services.AddScoped<IAlbumRepository, AlbumRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<ILoggingRepository, LoggingRepository>();
+
+            // Services
+            services.AddScoped<IMembershipService, MembershipService>();
+            services.AddScoped<IEncryptionService, EncryptionService>();
+
+            services.AddAuthentication();
+
+            // Polices
+            services.AddAuthentication(options =>
+            {
+                // inline policies
+                options.AddPolicy("AdminOnly", policy => { policy.RequireClaim(ClaimTypes.Role, "Admin"); });
+            });
+
+            // Add MVC servies to the services container
+            services.AddMvc().AddJsonOptions(opt =>
+            {
+                var resolver = opt.SerializerSettings.ContractResolver;
+                if (resolver != null)
+                {
+                    var res = resolver as DefaultContractResolver;
+                    res.NamingStrategy = null;
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             //loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // this will serve up wwwroot.
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            //AutoMapperConfiguration.Configure();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { AutomaticAuthenticate = true, AutomaticChallenge = true });
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
             });
+
+            DbInitializer.Initialize(app.ApplicationServices, applicationPath);
+        }
+
+        /// <summary>
+        /// Entry point for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
