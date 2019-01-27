e3664157b4b3df0a753c1e3bb964ae7892f36d5d
ducdmn <ndminhduc@gmail.com>
2019-01-27 11:25:11 +0700

    Long time no update

---
+++ src/PhotoGallery/wwwroot/app/core/services/membership.service.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=membership.service.js.map
---
+++ src/PhotoGallery/wwwroot/app/core/services/utility.service.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class UtilityService {
+    private _route: Router;
+
+    constructor(router: Router) {
+        this._route = router;
+    }
+
+    convertDateTime(date: Date) {
+        var formattedDate = new Date(date.toString());
+        return formattedDate.toDateString();
+    }
+
+    navigate(path: string) {
+        this._route.navigate([path]);
+    }
+
+    navigateToSignIn() {
+        this.navigate('/account/login');
+    }
+}
